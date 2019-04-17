5a4492fe0ab25c9991f8ba0912b88ae08753c1d2
lafleurc <you@example.com>
2019-04-17 15:25:29 -0400

    My Django Girls app, first commit

---
+++ hi.py
@@ -0,0 +1,12 @@
+# print('I am here')
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls=['Moi','Toi','Lui','Elle']
+for name in girls:
+    hi(name)
+    print('Next girl')
+# def hi():
+#   print('How are you?')
+#
+# hi()
